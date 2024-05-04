c8e3e80e7e9914c2e4deb1348941777e8979e135
reddemma <148742301+reddyreddemma@users.noreply.github.com>
2024-05-04 19:48:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function appendToScreen(value) {
+    document.getElementById('screen').value += value;
+  }
+  
+  function clearScreen() {
+    document.getElementById('screen').value = '';
+  }
+  
+  function calculate() {
+    let result = eval(document.getElementById('screen').value);
+    document.getElementById('screen').value = result;
+  }
+  
